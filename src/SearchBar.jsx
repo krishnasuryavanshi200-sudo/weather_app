2cff9ba1336199962e1b43344b8e757502f4895e
krishnasuryavanshi200-sudo <krishnasuryavanshi200@gmail.com>
2025-09-22 22:44:16 +0530

    Add files via upload

---
+++ src/SearchBar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const SearchBar = ({ fetchWeather }) => {
+    const[city,setCity]=useState("");
+     const handleSubmit = (e) => { 
+      e.preventDefault();
+    if (city.trim()) {
+      fetchWeather(city);
+      setCity("");
+    }
+  };
+  return(
+    <form className="flex" onSubmit={handleSubmit}>
+        
+      <input
+        type="text"
+        placeholder="Enter city name"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}className="flex-1 p-1 border border-gray-300 rounded-l-lg border-r-0"> 
+        </input>
+         <button
+        className="bg-blue-500 border cursor-pointer p-2 hover:bg-blue-600 border-l-0 rounded-r-lg"
+        type="submit"
+      >
+        Search
+      </button>
+        
+    </form>
+  )
+}
+
+export default SearchBar
